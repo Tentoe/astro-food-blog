63ed357e8de2ac0cd79526adddbfa1e99f96def6
Tentoe <tentoe86@pm.me>
2024-10-07 19:37:36 +0200

    sorted and split in days

--- util/convert.py
+++ util/convert.py
@@ -1,6 +1,7 @@
 
-from datetime import datetime
+from datetime import datetime, date
 import geojson
+from geojson import LineString
 import json
 
 geoJson = {}
@@ -21,6 +22,33 @@ point = features[0]
 
 sortedGeo = sorted(features, key = lambda x: x['properties']['timestamp'])
 
+days = [
+    date(2024, 9, 8),
+    date(2024, 9, 9),
+    date(2024, 9, 10),
+    date(2024, 9, 11),
+    date(2024, 9, 12),
+    date(2024, 9, 13),
+    date(2024, 9, 14),
+    date(2024, 9, 15),
+]
 
-print(datetime.fromtimestamp(sortedGeo[0]['properties']['timestamp']))
-print(datetime.fromtimestamp(sortedGeo[-1]['properties']['timestamp']))
+split = map(lambda y: filter(lambda x: datetime.fromtimestamp(x['properties']['timestamp']).date() == y, sortedGeo), days)
+
+
+# print(datetime.fromtimestamp(sortedGeo[0]['properties']['timestamp']))
+# print(datetime.fromtimestamp(sortedGeo[-1]['properties']['timestamp']))
+
+lines = [LineString([(point['geometry']['coordinates'][0], point['geometry']['coordinates'][1]) for point in day]) for day in split]
+
+# for day in split:
+    
+    # line = [(point['geometry']['coordinates'][0], point['geometry']['coordinates'][1]) for point in day] 
+  
+# out = LineString(line)  
+
+
+
+with open('out.json', 'w') as json_data:
+    geojson.dump(lines[0] ,json_data)
+
